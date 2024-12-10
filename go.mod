d603128853eebf2b121a54db749e95f6a332648a
Ivan Adi Saputra <ivanadisaputra34@gmail.com>
2024-12-10 19:18:41 +0700

    push redis

--- go.mod
+++ go.mod
@@ -8,7 +8,9 @@ require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/cbroglie/mustache v1.4.0 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -34,6 +36,7 @@ require (
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/redis/go-redis/v9 v9.7.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
---
+++ redis/redis_test.go
@@ -0,0 +1,206 @@
+package redis
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+	"github.com/stretchr/testify/assert"
+)
+
+var client = redis.NewClient(&redis.Options{
+	Addr: "localhost:6379",
+	DB:   0,
+})
+
+func TestConnection(t *testing.T) {
+	assert.NotNil(t, client)
+
+	//err := client.Close()
+	//assert.Nil(t, err)
+}
+
+var ctx = context.Background()
+
+func TestPing(t *testing.T) {
+	result, err := client.Ping(ctx).Result()
+	assert.Nil(t, err)
+	assert.Equal(t, "PONG", result)
+}
+
+func TestString(t *testing.T) {
+	client.SetEx(ctx, "name", "Ivan Adi", 3*time.Second)
+
+	result, err := client.Get(ctx, "name").Result()
+	assert.Nil(t, err)
+	assert.Equal(t, "Ivan Adi", result)
+
+	time.Sleep(5 * time.Second)
+
+	result, err = client.Get(ctx, "name").Result()
+	assert.NotNil(t, err)
+}
+
+func TestList(t *testing.T) {
+	client.RPush(ctx, "names", "Ivan")
+	client.RPush(ctx, "names", "Adi")
+	client.RPush(ctx, "names", "Saputra")
+
+	assert.Equal(t, "Ivan", client.LPop(ctx, "names").Val())
+	assert.Equal(t, "Adi", client.LPop(ctx, "names").Val())
+	assert.Equal(t, "Saputra", client.LPop(ctx, "names").Val())
+
+	client.Del(ctx, "names")
+}
+
+func TestSet(t *testing.T) {
+	client.SAdd(ctx, "students", "Ivan")
+	client.SAdd(ctx, "students", "Ivan")
+	client.SAdd(ctx, "students", "Adi")
+	client.SAdd(ctx, "students", "Adi")
+	client.SAdd(ctx, "students", "Saputra")
+	client.SAdd(ctx, "students", "Saputra")
+
+	assert.Equal(t, int64(3), client.SCard(ctx, "students").Val())
+	assert.Equal(t, []string{"Ivan", "Adi", "Saputra"}, client.SMembers(ctx, "students").Val())
+}
+
+func TestSortedSet(t *testing.T) {
+	client.ZAdd(ctx, "scores", redis.Z{Score: 100, Member: "Ivan"})
+	client.ZAdd(ctx, "scores", redis.Z{Score: 85, Member: "Budi"})
+	client.ZAdd(ctx, "scores", redis.Z{Score: 95, Member: "Joko"})
+
+	assert.Equal(t, []string{"Budi", "Joko", "Ivan"}, client.ZRange(ctx, "scores", 0, -1).Val())
+
+	assert.Equal(t, "Ivan", client.ZPopMax(ctx, "scores").Val()[0].Member)
+	assert.Equal(t, "Joko", client.ZPopMax(ctx, "scores").Val()[0].Member)
+	assert.Equal(t, "Budi", client.ZPopMax(ctx, "scores").Val()[0].Member)
+}
+
+func TestHash(t *testing.T) {
+	client.HSet(ctx, "user:1", "id", "1")
+	client.HSet(ctx, "user:1", "name", "Ivan")
+	client.HSet(ctx, "user:1", "email", "Ivan@example.com")
+
+	user := client.HGetAll(ctx, "user:1").Val()
+
+	assert.Equal(t, "1", user["id"])
+	assert.Equal(t, "Ivan", user["name"])
+	assert.Equal(t, "Ivan@example.com", user["email"])
+
+	client.Del(ctx, "user:1")
+}
+
+func TestGeoPoint(t *testing.T) {
+	client.GeoAdd(ctx, "sellers", &redis.GeoLocation{
+		Name:      "Toko A",
+		Longitude: 106.818489,
+		Latitude:  -6.178966,
+	})
+	client.GeoAdd(ctx, "sellers", &redis.GeoLocation{
+		Name:      "Toko B",
+		Longitude: 106.821568,
+		Latitude:  -6.180662,
+	})
+
+	distance := client.GeoDist(ctx, "sellers", "Toko A", "Toko B", "km").Val()
+	assert.Equal(t, 0.3892, distance)
+
+	sellers := client.GeoSearch(ctx, "sellers", &redis.GeoSearchQuery{
+		Longitude:  106.819143,
+		Latitude:   -6.180182,
+		Radius:     5,
+		RadiusUnit: "km",
+	}).Val()
+
+	assert.Equal(t, []string{"Toko A", "Toko B"}, sellers)
+}
+
+func TestHyperLogLog(t *testing.T) {
+	client.PFAdd(ctx, "visitors", "Ivan", "Adi", "Saputra")
+	client.PFAdd(ctx, "visitors", "Ivan", "budi", "joko")
+	client.PFAdd(ctx, "visitors", "rully", "budi", "joko")
+
+	total := client.PFCount(ctx, "visitors").Val()
+	assert.Equal(t, int64(6), total)
+}
+
+func TestPipeline(t *testing.T) {
+	_, err := client.Pipelined(ctx, func(pipeliner redis.Pipeliner) error {
+		pipeliner.SetEx(ctx, "name", "Ivan", 5*time.Second)
+		pipeliner.SetEx(ctx, "address", "Indonesia", 5*time.Second)
+		return nil
+	})
+	assert.Nil(t, err)
+
+	assert.Equal(t, "Ivan", client.Get(ctx, "name").Val())
+	assert.Equal(t, "Indonesia", client.Get(ctx, "address").Val())
+}
+
+func TestTransaction(t *testing.T) {
+	_, err := client.TxPipelined(ctx, func(pipeliner redis.Pipeliner) error {
+		pipeliner.SetEx(ctx, "name", "Joko", 5*time.Second)
+		pipeliner.SetEx(ctx, "address", "Cirebon", 5*time.Second)
+		return nil
+	})
+	assert.Nil(t, err)
+
+	assert.Equal(t, "Joko", client.Get(ctx, "name").Val())
+	assert.Equal(t, "Cirebon", client.Get(ctx, "address").Val())
+}
+
+func TestPublishStream(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		err := client.XAdd(ctx, &redis.XAddArgs{
+			Stream: "members",
+			Values: map[string]interface{}{
+				"name":    "Ivan",
+				"address": "Indonesia",
+			},
+		}).Err()
+		assert.Nil(t, err)
+	}
+}
+
+func TestCreateConsumerGroup(t *testing.T) {
+	client.XGroupCreate(ctx, "members", "group-1", "0")
+	client.XGroupCreateConsumer(ctx, "members", "group-1", "consumer-1")
+	client.XGroupCreateConsumer(ctx, "members", "group-1", "consumer-2")
+}
+
+func TestConsumeStream(t *testing.T) {
+	streams := client.XReadGroup(ctx, &redis.XReadGroupArgs{
+		Group:    "group-1",
+		Consumer: "consumer-1",
+		Streams:  []string{"members", ">"},
+		Count:    2,
+		Block:    5 * time.Second,
+	}).Val()
+
+	for _, stream := range streams {
+		for _, message := range stream.Messages {
+			fmt.Println(message.ID)
+			fmt.Println(message.Values)
+		}
+	}
+}
+
+func TestSubscribePubSub(t *testing.T) {
+	subscriber := client.Subscribe(ctx, "channel-1")
+	defer subscriber.Close()
+	for i := 0; i < 10; i++ {
+		message, err := subscriber.ReceiveMessage(ctx)
+		assert.Nil(t, err)
+		fmt.Println(message.Payload)
+	}
+}
+
+func TestPublishPubSub(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		err := client.Publish(ctx, "channel-1", "Hello "+strconv.Itoa(i)).Err()
+		assert.Nil(t, err)
+	}
+}
